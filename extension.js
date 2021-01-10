383d7ef6a84f0e3bbf6b61f6a6e74f774b3ab786
JCSergent <jcsergent20@gmail.com>
2021-01-09 20:29:33 -0500

    Feat: Implemented Two Commands. Save: saves the selectors current location. Load: moves the selector to the previously saved location

--- extension.js
+++ extension.js
@@ -1,6 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+const { Console } = require('console');
 const vscode = require('vscode');
+const { resolveCliPathFromVSCodeExecutablePath } = require('vscode-test');
+const editor = vscode.window.activeTextEditor;
+var savedPosition = new vscode.Position(0,0);
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,21 +14,45 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "coMemories" is now active!');
+	console.log('Crystal Of Memories Activated');
+
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('coMemories.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let saveLocation = vscode.commands.registerCommand('coMemories.saveCrystal', function () {
+	
+		vscode.window.showInformationMessage('Saving the Crystals Location');
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CrystalOfMemories!');
+		if(editor.selection.isEmpty) {
+			savedPosition = editor.selection.active;
+		}
+
+		console.log("Saving Cursor Position at:");
+		console.log("Line: "+savedPosition.line);
+		console.log("Character: "+savedPosition.character);
+		console.log(savedPosition);
+
 	});
 
-	context.subscriptions.push(disposable);
+	let loadLocation = vscode.commands.registerCommand('coMemories.useCrystal', function () {
+
+		vscode.window.showInformationMessage('Going to the Crystals Location');
+
+		var newPosition = savedPosition.with(savedPosition.line, savedPosition.character);
+		var newSelection = new vscode.Selection(newPosition, newPosition);
+		editor.selection = newSelection;
+
+		console.log("Using Crystal of Memories");
+		console.log(newPosition);
+
+	});
+
+
+
+
+
+	context.subscriptions.push(saveLocation, loadLocation);
 }
 
 exports.activate = activate;
